46c73196e36037df9ae929a104b359cf6fdfc750
No7_dv <jeux-app-eve_7@outlook.fr>
2024-11-07 14:04:14 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,14 @@
+// function myFunction () {
+//     var element = document.body
+//     var buttonToggle = document.querySelector('div')
+//     element.addEventListener('click', ()=> {
+//         buttonToggle.classList.toggle('darkButton')
+//         element.classList.toggle('darkMode')
+//     })
+// }
+
+function myFunction () {
+    var element = document.body;
+    element.classList.toggle('darkMode')
+    
+}
